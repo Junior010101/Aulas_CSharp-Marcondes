e22fee29b962ff7a8706096a63ac370c6a232e81
Junior010101 <marcondesjunior2209@gmail.com>
2024-09-12 02:17:54 -0300

    Adição da aula02 e da atividade01

---
+++ Aula01/Aula01.cs
@@ -0,0 +1,19 @@
+﻿//Importações
+using System;
+
+//Operações: soma, subtração, multiplicação e divisão
+Console.WriteLine("Demonstração de Soma");
+
+Console.WriteLine("Digite um numero");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro numero");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma entre "+ num + " e " + num2 +" é "+ (num+num2));
+Console.WriteLine("A subtração entre "+ num + " e " + num2 +" é "+ (num  - num2));
+Console.WriteLine("A multiplicação entre "+ num + " e " + num2 +" é "+ (num * num2));
+Console.WriteLine("A divisão entre "+ num + " e " + num2 +" é "+ (num / num2));
+
+// Na aula 01, fomos apresentados aos metodos de emtrada e saida de texto no console.
+// Depois vimos os operadores numericos.
---
+++ Aula02/Aula02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("------------Algoritimo que exibe se um numero é par ou Impar------------");
+
+Console.WriteLine("Digite um numero:");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0){
+    Console.WriteLine("O Numero Inserido é Par");
+}
+else{
+    Console.WriteLine("O Numero Inserido é Impar");
+}
+
+// Nesta aula aprendemos um pouco sobre condicionais.
+// Vimos a tabela verdade, utilizamos as condições IF (Se) e Else (Se Não).
